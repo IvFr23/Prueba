d95358dd3a79075184eaa15b45e19aa0cc4065c4
IvFr23 <ivanfran2015@gmail.com>
2024-01-03 18:43:28 +0100

    Dates

---
+++ src/OperadoresAritmeticos.java
@@ -0,0 +1,19 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        /*variable de tipo in para realizar las operaciones
+        de suma
+        resta
+        multiplicacion
+        division
+        y modulo.
+         */
+        int num1 = 5 ;
+        int num2 = 4 ;
+        //imprimir en consola.
+        System.out.println("La suma es: " + (num1 + num2));
+        System.out.println("La resta es: " + (num1 - num2));
+        System.out.println("La multiplicacion es: " + (num1 * num2));
+        System.out.println("La division es: " + (num1 / num2));
+        System.out.println("El modulo es: " + (num1 % num2));
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+ public static void main(String[] args){
+     //variable tipo int de nombre telefono y esta inicializada.
+     int telefono = 958988420;
+     //vaeiable tipo char.
+     char letra = 'a';
+     /*variable tipo float
+     nombre: peso.
+     recuerda es necesario hacer un casteo
+     puede ser con la letra "f".
+      */
+     float peso = 65.5f;
+     double ngrande = 38.5522457587584725285728547847584;
+     /*imprimir en consola
+     para ahorar trabajo escribe
+     sout como atajo.
+      */
+     System.out.println("Numero de telefono: " + telefono);
+     System.out.println("Letra: " + letra);
+     System.out.println("Peso: " + peso);
+     System.out.println("Numero grande: " + ngrande);
+ }
+}
