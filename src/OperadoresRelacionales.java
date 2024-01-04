2f28b02d998d6062ae4eea4da0eeb78b9dbb0dc6
IvFr23 <ivanfran2015@gmail.com>
2024-01-04 11:14:36 +0100

    Dates

---
+++ src/OperadoresRelacionales.java
@@ -0,0 +1,20 @@
+public class OperadoresRelacionales {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        //Operaciones relacionales
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a > b: " + (a >= b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a != b: " + (a != b));
+        /*
+        Recuerda que los operadores relacionales sirven
+        para comparar valores y obtener resultados logicos,
+        estos influyen en la toma de decisiones de nuestra
+        computadora.
+         */
+    }
+}
